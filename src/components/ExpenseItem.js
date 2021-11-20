21ba9affd2c1e19dbb59f77cc8da4138ac4122af
chiahui <ashleyhsueh33@gmail.com>
2021-11-20 17:05:24 +0800

    feat: add ExpenseList components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,6 @@ import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' })
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-  const year = props.date.getFullYear()
-
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
