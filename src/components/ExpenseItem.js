f3e932e08253a967fe2e49b901df5eb76b4943d2
chiahui <ashleyhsueh33@gmail.com>
2021-11-20 16:39:04 +0800

    feat: Splitting Components Into Multiple Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
@@ -7,11 +8,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
