2009ff3c59e037c1ecc087d64b723610b4f1cd6a
chiahui <ashleyhsueh33@gmail.com>
2021-11-27 17:39:15 +0800

    feat: onClick event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,10 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
-function ExpenseItem(props) {
+const ExpenseItem = props => {
+  const clickHandler = () => {
+    console.log('clicked!')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
